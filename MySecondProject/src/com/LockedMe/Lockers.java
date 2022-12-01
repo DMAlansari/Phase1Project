45bd0d958d65e637403d3cb17b95b7fa3b372ca6
DMAlansari <Dalal.m.alansari@gmail.com>
2022-12-01 18:00:40 +0300

    updating the code

--- MySecondProject/src/com/LockedMe/Lockers.java
+++ MySecondProject/src/com/LockedMe/Lockers.java
@@ -11,10 +11,7 @@ public class Lockers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//---------------------------------------------------------------------------------------------------------------------
-//		List<UserFiles> files = new ArrayList<>();
-//		files.add(new UserFiles("music.txt"));
-//		files.add(new UserFiles("videos.txt"));
+
 		int status=0;
 		List<String> files = new ArrayList<>();
 		files.add("music.txt");
@@ -23,44 +20,11 @@ public class Lockers {
 		new UserFiles().createFile(files.get(1));
 		status = new UserFiles().deleteFile(files.get(0));
 		System.out.println(status);
+		if(status==1) {
+			files.remove(0);
+		}
 		
-		//File file = new File("test.txt"); // creating a file   
-//boolean result;  
-//try   
-//{  
-//result = file.createNewFile();  //creates a new file  
-//if(result)      // test if successfully created a new file  
-//{  
-//System.out.println("file created "+file.getCanonicalPath()); //returns the path string  
-//}  
-//else  
-//{  
-//System.out.println("File already exist at location: "+file.getCanonicalPath());  
-//}  
-//}   
-//catch (IOException e)   
-//{  
-//e.printStackTrace();    //prints exception if any  
-//} // end of creating a file
-////---------------------------------------------------------------------------------------------------------------------
-//try  // delete files
-//{         
-//	 file = new File("C:\\Users\\Dalal\\eclipse-workspace\\MySecondProject\\test.txt");          //file to be delete  
-//if(file.delete())                      //returns Boolean value  
-//{  
-//System.out.println(file.getName() + " deleted");   //getting and printing the file name  
-//}  
-//else  
-//{  
-//System.out.println("failed");  
-//}  
-//}  
-//catch(Exception e)  
-//{  
-//e.printStackTrace();  
-//} // end of delete files
-//---------------------------------------------------------------------------------------------------------------------
-// creating 	
+
 	}
 	
 
