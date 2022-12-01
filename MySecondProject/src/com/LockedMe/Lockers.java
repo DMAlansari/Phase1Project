15dfc7d993b337d072a8b067982d702ce987673d
DMAlansari <Dalal.m.alansari@gmail.com>
2022-12-01 22:10:51 +0300

    added greeting msg and switch case for the menu

--- MySecondProject/src/com/LockedMe/Lockers.java
+++ MySecondProject/src/com/LockedMe/Lockers.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 
 
@@ -14,8 +15,49 @@ public class Lockers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		// Apps name
+		int usersOption=0;
+		System.out.println("Hello!, here at LockedMe application we make it easy for you to safley create your own files.");
+		System.out.println("Developed by Eng. Dalal Al-ansari");
+		System.out.println("Let us help you create, modify or even remove your files");
+		System.out.println("Kindly choose from the following to options");
+		System.out.println("To  add  a  new  file  type 1");
+		System.out.println("To   delete   a  file  type 2");
+		System.out.println("To  view   your  files type 3");
+		System.out.println("To search for a  file  type 4");
+		System.out.println("To return to home page type 5");
+		System.out.println("To exit type -1");
+		System.out.println("Type here:");
+		usersOption = scan.nextInt();
+		
+		switch (usersOption) {
+		case 1: {
+			
+			break;}
+		case 2: {
+			
+			break;}
+		case 3: {
+			
+			break;}
+		case 4: {
+			
+			break;}
+		case 5: {
+			
+			break;}
+		case -1: {
+			
+			break;}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + usersOption);
+		}
 
-		int status=0;
+
+		
+		
+// status=0;
 		List<String> files = new ArrayList<>();
 		files.add("music.txt");
 		files.add("videos.txt");
@@ -43,10 +85,7 @@ public class Lockers {
 		
 		
 		//status = new UserFiles().deleteFile(files.get(0));
-		System.out.println(status);
-		if(status==1) {
-			files.remove(0);
-		}
+
 		
 
 	}
@@ -83,7 +122,7 @@ public class Lockers {
 		}
 	 }
 	 
-	 public int deleteFile(String fileName) {
+	 public void deleteFile(String fileName) {
 		 int status=-1;
 		try  // delete files
 		{         
@@ -92,7 +131,7 @@ public class Lockers {
 		{  
 		System.out.println(file.getName() + " deleted");  
 		//getting and printing the file name  
-		status =1;
+		
 		}  
 		else  
 		{  
@@ -103,7 +142,7 @@ public class Lockers {
 		{  
 		e.printStackTrace();  
 		}
-		return status;
+		
 	 }
 	 
  }
